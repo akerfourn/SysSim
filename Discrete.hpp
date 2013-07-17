b56dda3ef10fdbb22900012a75b7f61a4f7db1bb
Adrien Kerfourn <akerfourn.forge@gmail.com>
2013-07-17 22:42:08 +0200

    Ajouts d'un intégrateur pour système à temps discret [/!\ Non testé].

---
+++ Discrete.hpp
@@ -0,0 +1,38 @@
+#ifndef __EULER_HPP__
+#define __EULER_HPP__
+
+/* 
+ * Intégrateur pour système discret x[n+1] = f(t,x[n]) (x = dx).
+ */
+
+#include "Integrators.hpp"
+
+template<typename T>
+class Discrete: public FixedStepIntegrator<T>
+{
+	public:
+		Discrete(void):FixedStepIntegrator<T>(){};
+		Discrete(T step):FixedStepIntegrator<T>(step){};
+		Discrete(FixedStepIntegrator<T> &other):FixedStepIntegrator<T>(other){};
+
+		void operator()(T &t, DynamicalSystem<T> &system);
+};
+
+template<typename T>
+void Discrete<T>::operator()(T &t, DynamicalSystem<T> &system)
+{
+	long i;
+
+	system.f(t,system);
+
+	for(i = 0; i < system.sizex(); ++i)
+	{
+		system[i] = system.getdx(i);
+	}
+
+	t = t + this->step;
+
+	system.h(t,system);
+}
+
+#endif
