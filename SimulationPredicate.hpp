9b1c6ea7c48e54439a48e8a0cb413c85ef1ce1f9
Adrien Kerfourn <akerfourn.forge@gmail.com>
2014-03-18 22:31:02 +0100

    Simplification of SimulationPredicate elements.

--- SimulationPredicate.hpp
+++ SimulationPredicate.hpp
@@ -20,54 +20,38 @@ class SimulationPredicate
 		virtual bool operator()(void) = 0;
 };
 
+
+
 template<typename T>
 class IterativePredicate: public SimulationPredicate
 {
 	protected:
-		unsigned long nbskipedpoints;
 		unsigned long nbpoints;
 		unsigned long i;
 
 		bool firststep;
 	public:
-		IterativePredicate(unsigned long nbpoints, unsigned long nbskipedpoints = (unsigned long)0);
+		IterativePredicate(unsigned long nbpoints);
 		virtual ~IterativePredicate(void){};
 
 		virtual bool operator()(void);
 };
 
 template<typename T>
-IterativePredicate<T>::IterativePredicate(unsigned long nbpoints, unsigned long nbskipedpoints = (unsigned long)0)
+IterativePredicate<T>::IterativePredicate(unsigned long nbpoints)
 {
-	this->nbskipedpoints = nbskipedpoints+1;
 	this->nbpoints = nbpoints+1;
 	this->i = 0;
-	this->firststep = true;
 }
 
 template<typename T>
 inline bool operator()(void)
 {
 	++(this->i);
-
-	if (this->firststep)
-	{
-		if (this->i < this->nbskipedpoints)
-		{
-			return true;
-		}
-		else
-		{
-			this->i = 0;
-			this->firstep = false;
-			return false;
-		}
-	}
-	else
-	{
-		return (this->i < this->nbpoints);
-	}
+	return (this->i < this->nbpoints);
 }
+
+
 
 template<typename T>
 class TimePredicate<T>: public SimulationPredicate
@@ -75,12 +59,10 @@ class TimePredicate<T>: public SimulationPredicate
 	protected:
 		T *t;
 		
-		T ti, tf;
-
-		bool firststep;
+		T duration;
 
 	public:
-		TimePredicate(T &t, T tf, T ti = (T)0.0);
+		TimePredicate(T &t, T duration);
 		virtual ~TimePredicate(void){};
 
 		virtual bool operator()(void);
@@ -88,33 +70,16 @@ class TimePredicate<T>: public SimulationPredicate
 };
 
 template<typename T>
-TimePredicate<T>::TimePredicate(T &t, T tf, T ti = (T)0.0)
+TimePredicate<T>::TimePredicate(T &t, T duration)
 {
-	this->ti = ti;
-	this->tf = tf;
+	this->duration = duration;
 	this->t = &t;
-	this->>firststep = true;
 }
 
 template<typename T>
 inline bool operator()(void)
 {
-	if (firststep)
-	{
-		if (*this->t < ti)
-		{
-			return true;
-		}
-		else
-		{
-			this->firststep = false;
-			return false;
-		}
-	}
-	else
-	{
-		return (*this->t < tf);
-	}
+	return (*this->t < this->duration);
 }
 
 
