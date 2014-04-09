8cdc0020ba4a5048902d1d47579785f4f67afd4f
Adrien Kerfourn <akerfourn.forge@gmail.com>
2014-04-09 22:45:27 +0200

    Test of the Rossler network.

--- examples/try/try.cpp
+++ examples/try/try.cpp
@@ -6,6 +6,7 @@
 #include "Euler.hpp"
 #include "Simulation.hpp"
 #include "Network.hpp"
+#include "RosslerConnection.hpp"
 
 int main(void)
 {
@@ -20,16 +21,36 @@ int main(void)
 
 	Simulation<double> sim(network,integrator);
 
+	RosslerConnection<double> c1(network,0,3,5e-1);
+	RosslerConnection<double> c2(network,3,6,5e-1);
+	RosslerConnection<double> c3(network,6,0,5e-1);
+
 	network.add(ross1);
 	network.add(ross2);
 	network.add(ross3);
 
+	ross1.add(c3);
+	ross2.add(c1);
+	ross3.add(c2);
 
-	for(long i = 0; i < network.sizex(); ++i)
-	{
-		network[i] = (double)0.0;
-	}
 
+	// 1.85   0.42   1.07
+	// 1.88   0.67   2.86
+	// 0.02   0.71   0.89
+
+	network[0] = (double)1.85;
+	network[1] = (double)0.42;
+	network[2] = (double)1.07;
+
+	network[3] = (double)1.88;
+	network[4] = (double)0.67;
+	network[5] = (double)2.86;
+
+/*
+	network[6] = (double)0.02;
+	network[7] = (double)0.71;
+	network[8] = (double)0.89;
+*/
 	double ti = 0.0;
 	double tf = 200.0;
 	long npts = 10000;
