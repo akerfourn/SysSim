dd76a00507700f8e4187bf2072de20fff6ea5dd9
Adrien Kerfourn <akerfourn.forge@gmail.com>
2014-04-18 21:54:45 +0200

    Adds CeCILL-C license.

--- examples/SimpleRosslerNet/LRossler.hpp
+++ examples/SimpleRosslerNet/LRossler.hpp
@@ -1,35 +1,9 @@
 #ifndef __LROSSLER_HPP__
 #define __LROSSLER_HPP__
 
-/*	LRossler.hpp
+/*	LRossler
  *
- * Copyright Adrien KERFOURN (2014)
- *
- * This software is governed by the CeCILL-C license under French law and
- * abiding by the rules of distribution of free software.  You can  use, 
- * modify and/ or redistribute the software under the terms of the CeCILL-C
- * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info". 
- * 
- * As a counterpart to the access to the source code and  rights to copy,
- * modify and redistribute granted by the license, users are provided only
- * with a limited warranty  and the software's author,  the holder of the
- * economic rights,  and the successive licensors  have only  limited
- * liability. 
- * 
- * In this respect, the user's attention is drawn to the risks associated
- * with loading,  using,  modifying and/or developing or reproducing the
- * software by the user in light of its specific status of free software,
- * that may mean  that it is complicated to manipulate,  and  that  also
- * therefore means  that it is reserved for developers  and  experienced
- * professionals having in-depth computer knowledge. Users are therefore
- * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or 
- * data to be ensured and,  more generally, to use and operate it in the 
- * same conditions as regards security. 
- * 
- * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL-C license and that you accept its terms.
+ * Adrien KERFOURN
  *
  */
 
@@ -42,15 +16,19 @@ class LRossler: public LocalSystem<T>
 		T a, b, c;
 
 	public:
+		using LocalSystem<T>::dx;
+		using LocalSystem<T>::x;
+		using typename LocalSystem<T>::size_type;
+
 		LRossler(void);
 		LRossler(T a, T b, T c);
 
 		inline void changeparameters(T a, T b, T c);
 
-		virtual void localf(T t, SystemStates<T>& x, DynamicalSystem<T>& network, long basex, long basey);
+		virtual void localf(T t);
 
-		virtual long sizex(void){return (long)3;};
-		virtual long sizey(void){return (long)0;};
+		virtual size_type sizex(void){return (size_type)3;};
+		virtual size_type sizey(void){return (size_type)0;};
 
 };
 
@@ -80,16 +58,18 @@ inline void LRossler<T>::changeparameters(T a, T b, T c)
 
 
 template<typename T>
-void LRossler<T>::localf(T t, SystemStates<T>& x, DynamicalSystem<T>& network, long basex, long basey)
+void LRossler<T>::localf(T t)
 {
+
 	T u = 0;
-	for (int i = 0; i < this->neighbors.size(); ++i)
+	for (int i = 0; i < this->sizen(); ++i)
 	{
 		u += this->get(i)();
 	}
-	network.dx(basex) = -x[basex+1] - x[basex+2] + u;
-	network.dx(basex+1) = x[basex+0] + a * x[basex+1];
-	network.dx(basex+2) = b + x[basex+2] * ( x[basex+0] - c);
+
+	dx(0) = -x(1) - x(2) + u;
+	dx(1) = x(0) + a * x(1);
+	dx(2) = b + x(2) * ( x(0) - c);
 }
 
 
