513230029d5206b1ae16a9dcbcc025af184cc386
Adrien Kerfourn <akerfourn.forge@gmail.com>
2014-05-01 23:48:28 +0200

    Update SimpleRosslerNet example.

--- examples/SimpleRosslerNet/LRossler.hpp
+++ examples/SimpleRosslerNet/LRossler.hpp
@@ -44,33 +44,27 @@ class LRossler: public LocalSystem<T>
 	public:
 		using LocalSystem<T>::dx;
 		using LocalSystem<T>::x;
-		using typename LocalSystem<T>::size_type;
+		typedef typename LocalSystem<T>::size_type size_type;
 
-		LRossler(void);
-		LRossler(T a, T b, T c);
+		LRossler(void):LocalSystem<T>()
+		{
+			this->changeparameters( (T)0.432, (T)2.0, (T)4.0 );
+			return;
+		}
+		LRossler(T a, T b, T c):LocalSystem<T>()
+		{
+			this->changeparameters(a, b, c);
+			return;
+		}
 
 		inline void changeparameters(T a, T b, T c);
 
 		virtual void localf(T t);
 
-		virtual size_type sizex(void){return (size_type)3;};
-		virtual size_type sizey(void){return (size_type)0;};
+		virtual inline size_type sizex(void){return (size_type)3;};
+		virtual inline size_type sizey(void){return (size_type)0;};
 
 };
-
-template<typename T>
-LRossler<T>::LRossler(void)
-{
-	this->changeparameters( (T)0.432, (T)2.0, (T)4.0 );
-	return;
-}
-
-template<typename T>
-LRossler<T>::LRossler(T a, T b, T c)
-{
-	this->changeparameters(a, b, c);
-	return;
-}
 
 
 template<typename T>
@@ -86,16 +80,14 @@ inline void LRossler<T>::changeparameters(T a, T b, T c)
 template<typename T>
 void LRossler<T>::localf(T t)
 {
-
-	T u = 0;
-	for (int i = 0; i < this->sizen(); ++i)
-	{
-		u += this->get(i)();
-	}
-
-	dx(0) = -x(1) - x(2) + u;
+	dx(0) = -x(1) - x(2);
 	dx(1) = x(0) + a * x(1);
 	dx(2) = b + x(2) * ( x(0) - c);
+
+	for (int i = 0; i < this->sizen(); ++i)
+	{
+		dx(0) += this->get(i)();
+	}
 }
 
 
